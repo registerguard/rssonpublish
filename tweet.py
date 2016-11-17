3222208bc10918d79057402addd73e7968b496fa
Rob Denton <robdentonrg@gmail.com>
2016-11-16 18:06:57 -0800

    Default to no tweet

--- tweet.py
+++ tweet.py
@@ -27,7 +27,7 @@ def sendit(feed_url, feed_title):
     try:
         # Comment out line below to not send tweet each test...
         # would be nice to make this a test variable or something
-        api.update_status(status=tweet_text)
+        #api.update_status(status=tweet_text)
         success = True
         #logger.debug('Success! Tweet sent: ' + tweet_text)
     except tweepy.TweepError, err:
