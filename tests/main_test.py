5ae254369453f9e16c192673e3ed15228d76d552
Rob Denton <robdentonrg@gmail.com>
2016-11-21 18:06:16 -0800

    fix broken test

--- tests/main_test.py
+++ tests/main_test.py
@@ -7,9 +7,10 @@ def test_main():
     
     url = 'http://registerguard.com/csp/cms/sites/rg/feeds/rss.csp'
     payload = {'pub': 'rg', 'section': 'sports', 'area': 'Top%20Updates'}
+    id_file = "id_files/id_file_example.txt"
     
     # Try tweet
-    responses = checknsend(url,payload)
+    responses = checknsend(url,payload,id_file)
     
     for key, value in responses:
         assert ( value == "success")
